dde0eafaaa425bf831dd2a0053169a295cd1f401
BoyinaVamsiKrishna <52597000+BoyinaVamsiKrishna@users.noreply.github.com>
2024-11-27 20:49:42 +0530

    Add files via upload

---
+++ src/lib/idGenerator.ts
@@ -0,0 +1,7 @@
+// export function idGenerator(): string {
+//     return Math.floor(Math.random() * 10001).toString();
+//   }
+
+export function idGenerator(): string {
+    return Math.floor(Math.random() * 10001).toString();
+  }

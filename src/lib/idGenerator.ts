fa58f025e53a79658caec835e378dbd4dd8eab7c
BoyinaVamsiKrishna <52597000+BoyinaVamsiKrishna@users.noreply.github.com>
2024-12-01 00:32:41 +0530

    Delete src directory

--- src/lib/idGenerator.ts
+++
@@ -1,7 +0,0 @@
-// export function idGenerator(): string {
-//     return Math.floor(Math.random() * 10001).toString();
-//   }
-
-export function idGenerator(): string {
-    return Math.floor(Math.random() * 10001).toString();
-  }
